925fd93aa35bad3de1554b44c5d6a4acddfc03fc
Anthony Cowley <acowley@gmail.com>
2018-05-30 20:47:29 -0400

    rocm-cmake: update

--- pkgs/development/tools/rocm-cmake.nix
+++ pkgs/development/tools/rocm-cmake.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchFromGitHub, cmake }:
 stdenv.mkDerivation {
   name = "rocm-cmake";
-  version = "2018-03-30";
+  version = "2018-05-28";
   src = fetchFromGitHub {
     owner = "RadeonOpenCompute";
     repo = "rocm-cmake";
-    rev = "ec7313aa43de72729c8f66b2e53155e03aa74e20";
-    sha256 = "095x40az48nz28vm2azl7ybbddmp9c2x6nz26qdcl60h8mhqhrqx";
+    rev = "3f43e2d493f24abbab4dc189a9ab12cc3ad33baf";
+    sha256 = "0j12jbgi295y37ana86dy8rsys886blr09yd95clwjkrcxv9mgir";
   };
   nativeBuildInputs = [ cmake ];
 }
