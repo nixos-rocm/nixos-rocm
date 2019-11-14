0fbb674d643d1f01d7b78ca816b443b025178b9b
Anthony Cowley <acowley@gmail.com>
2019-11-14 00:23:52 -0500

    Embed the absolute path of ld.lld in hcc-clang
    This eliminates another way in which a ROCm tool was invoking ld.lld,
    which otherwise requires that it be included as a `buildInput` to make
    it available on `PATH` at runtime.

--- pkgs/development/compilers/hcc-clang/default.nix
+++ pkgs/development/compilers/hcc-clang/default.nix
@@ -33,6 +33,7 @@ stdenv.mkDerivation rec {
     sed 's/  ''${version_inc}//' -i lib/Basic/CMakeLists.txt
     sed 's/\([[:space:]]*\)\(cmake_policy(SET CMP0075 NEW)\)/\1\2\n\1cmake_policy(SET CMP0077 NEW)/' -i CMakeLists.txt
     sed 's|\([[:space:]]*const Twine e = \)rocm + "/bin/rocm_agent_enumerator";|\1"${rocminfo}/bin/rocm_agent_enumerator";|' -i lib/Driver/ToolChains/Hcc.cpp
+    sed 's|\([[:space:]]*std::string Linker = \)getToolChain().GetProgramPath(getShortName())|\1"${hcc-lld}/bin/ld.lld"|' -i lib/Driver/ToolChains/AMDGPU.cpp
   '';
   #    sed '/[[:space:]]*case GK_GFX908:/,/[[:space:]]*LLVM_FALLTHROUGH;/d' -i lib/Basic/Targets/AMDGPU.cpp
 
