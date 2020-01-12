ac0731d1a3e2f9faa80a7c650cb9414052e7c0c5
Anthony Cowley <acowley@gmail.com>
2020-01-12 10:53:07 -0500

    rocblas: compatibility with gcc-9.2.0

--- pkgs/development/libraries/rocblas/tensile.nix
+++ pkgs/development/libraries/rocblas/tensile.nix
@@ -1,4 +1,5 @@
-{ fetchFromGitHub, buildPythonPackage, pyyaml, pytest, lib, config, rocminfo, hcc, rocm-smi }:
+{ stdenv, fetchFromGitHub, buildPythonPackage, pyyaml, pytest, lib, config
+, rocminfo, hcc, rocm-smi }:
 buildPythonPackage rec {
   pname = "Tensile";
   version = "3.0.0";
@@ -11,12 +12,17 @@ buildPythonPackage rec {
   };
   buildInputs = [ pyyaml pytest ];
 
+  # The last patch restores compatibility with GCC 9.2's STL.
+  # See: https://github.com/ROCmSoftwarePlatform/rocBLAS/issues/845
+
   postPatch = ''
     sed -e 's|locateExe("/opt/rocm/bin", "rocm_agent_enumerator")|locateExe("${rocminfo}/bin", "rocm_agent_enumerator")|' \
         -e 's|locateExe("/opt/rocm/bin", "hcc");|locateExe("${hcc}/bin", "hcc")|' \
         -e 's|locateExe("/opt/rocm/bin", "rocm-smi")|locateExe("${rocm-smi}/bin", "rocm-smi")|' \
         -e 's|locateExe("/opt/rocm/bin", "extractkernel")|locateExe("${hcc}/bin", "extractkernel")|' \
         -i Tensile/Common.py
+  '' + lib.optionalString (stdenv.cc.isGNU && lib.versionAtLeast stdenv.cc.version "9.2") ''
+    sed 's|const Items empty;|const Items empty = {};|' -i Tensile/Source/lib/include/Tensile/EmbeddedData.hpp
   '';
 
   # We need patched source files in the output, so we can't symlink
