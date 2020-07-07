3fa3df229dc9aba7ecaad86244138a51f6288221
Holger Wünsche <holger.o.wuensche@t-online.de>
2020-07-07 17:04:12 +0200

    WIP fixed rocBLAS test
    NOTE: Not yet working for ROCm 3.5.
    
    The tests need to be run manually since a GPU is needed to run this test
    and it can't be found from inside the sandbox. The test-binary+data is
    added to the output when building with doCheck=true (it is false by
    default), this leads to an additional 650MB, so it only makes sense for
    testing. The test-binary is wrapped and can be called from the
    result-directory created by nix-build. (Be aware the test take a long
    time >6h for me).
    
    rocblas: fix build
    
    This was broken by the rocblas testing work.

--- pkgs/development/libraries/rocblas/default.nix
+++ pkgs/development/libraries/rocblas/default.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchFromGitHub, lib, config, cmake, pkgconfig, libunwind, python
 , rocm-runtime, hip-clang, rocm-cmake, comgr, clang
-, llvm, openmp
+, llvm, openmp, makeWrapper
 , doCheck ? false
 # Tensile slows the build a lot, but can produce a faster rocBLAS
 , useTensile ? true, rocblas-tensile ? null
-, gfortran, liblapack, boost, gtest }:
+, gfortran, liblapack, boost, gtest, openblas }:
 let pyenv = python.withPackages (ps:
                with ps; [pyyaml pip wheel setuptools virtualenv]); in
 assert useTensile -> rocblas-tensile != null;
@@ -17,10 +17,13 @@ stdenv.mkDerivation rec {
     rev = "rocm-${version}";
     sha256 = "13rbdd49byrddmahn4ac90nw0anpbgj547y651zf8hdpnhh4n7wp";
   };
+
+  inherit doCheck;
+
   nativeBuildInputs = [ cmake rocm-cmake pkgconfig python ];
 
   buildInputs = [ libunwind pyenv rocm-runtime comgr llvm openmp hip-clang ]
-                ++ stdenv.lib.optionals doCheck [ gfortran boost gtest liblapack ];
+                ++ stdenv.lib.optionals doCheck [ gfortran boost gtest liblapack openblas makeWrapper ];
 
   CXXFLAGS = "-D__HIP_PLATFORM_HCC__";
   cmakeFlags = [
@@ -31,9 +34,10 @@ stdenv.mkDerivation rec {
     "-DTensile_COMPILER=hipcc"
     ''-DAMDGPU_TARGETS=${lib.strings.concatStringsSep ";" (config.rocmTargets or ["gfx803" "gfx900" "gfx906"])}''
   ] ++ stdenv.lib.optionals doCheck [
-    "-DBUILD_CLIENTS_SAMPLES=YES"
+    "-DBUILD_CLIENTS_SAMPLES=NO"
     "-DBUILD_CLIENTS_TESTS=YES"
-    "-DBUILD_CLIENTS_BENCHMARKS=YES"
+    "-DBUILD_CLIENTS_BENCHMARKS=NO"
+    "-DLINK_BLIS=NO"
   ] ++ stdenv.lib.optionals useTensile [
     "-DVIRTUALENV_HOME_DIR=${rocblas-tensile}"
     "-DTensile_TEST_LOCAL_PATH=${rocblas-tensile}"
@@ -42,10 +46,38 @@ stdenv.mkDerivation rec {
     "-DTensile_LOGIC=hip_lite"
   ];
 
+  # sed '/add_custom_command(/,/^ )/d' -i library/src/CMakeLists.txt
   prePatch = ''
     patchShebangs ./header_compilation_tests.sh
     sed -e '/include(virtualenv)/d' \
         -e '/virtualenv_install.*/d' \
         -i CMakeLists.txt
+
+    substituteInPlace ./clients/benchmarks/CMakeLists.txt \
+        --replace 'cblas lapack roc::rocblas' 'blas cblas lapack roc::rocblas'
+
+    substituteInPlace ./clients/gtest/CMakeLists.txt \
+        --replace 'cblas lapack roc::rocblas' 'blas cblas lapack roc::rocblas'
+
+    patchShebangs ./clients/common/rocblas_gentest.py
+  '';
+
+  preBuild = stdenv.lib.optional doCheck ''
+    cp -r ../clients/gtest/ clients/
+  '';
+
+  # not used, because GPU is not available during building
+  #checkPhase = ''
+  #  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/library/src/"
+  #  clients/staging/rocblas-test
+  #'';
+
+  postInstall = stdenv.lib.optional doCheck ''
+    mkdir -p $out/bin/
+    mkdir -p $out/test/
+    cp clients/staging/rocblas-test $out/bin/
+    cp clients/staging/rocblas_gtest.data $out/test/
+
+    wrapProgram $out/bin/rocblas-test --add-flags "--data $out/test/rocblas_gtest.data"
   '';
 }
