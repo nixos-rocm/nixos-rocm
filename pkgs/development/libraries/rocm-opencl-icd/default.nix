e23b1a3bd535a96e73d8deffb70cdfd1ff66a76f
Anthony Cowley <acowley@gmail.com>
2020-08-22 00:16:51 -0400

    rocm-opencl-icd: set version to rocm-opencl-runtime.version
    By @danieldk

--- pkgs/development/libraries/rocm-opencl-icd/default.nix
+++ pkgs/development/libraries/rocm-opencl-icd/default.nix
@@ -1,12 +1,20 @@
-{ stdenv, rocm-opencl-runtime, writeText }:
+{ stdenv, rocm-opencl-runtime }:
 
 stdenv.mkDerivation rec {
-  version = "3.5.0";
-  name = "rocm-opencl-icd";
-  src = writeText "amdocl64.icd" "${rocm-opencl-runtime}/lib/libamdocl64.so";
-  unpackPhase = ":";
+  pname = "rocm-opencl-icd";
+  version = rocm-opencl-runtime.version;
+
+  dontUnpack = true;
+
   installPhase = ''
     mkdir -p $out/etc/OpenCL/vendors
-    cp $src $out/etc/OpenCL/vendors/amdocl64.icd
+    echo "${rocm-opencl-runtime}/lib/libamdocl64.so" > $out/etc/OpenCL/vendors/amdocl64.icd
   '';
+
+  meta = with stdenv.lib; {
+    description = "OpenCL ICD definition for AMD GPUs using the ROCm stack";
+    license = licenses.mit;
+    maintainers = with maintainers; [ danieldk ];
+    platforms = platforms.linux;
+  };
 }
