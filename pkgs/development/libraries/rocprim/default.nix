70a6887503123966cc297095c035af1b2488e02f
Wulfsta <wulfstawulfsta@gmail.com>
2020-09-23 21:46:38 -0400

    rocprim: update to 2.10.4

--- pkgs/development/libraries/rocprim/default.nix
+++ pkgs/development/libraries/rocprim/default.nix
@@ -1,29 +1,44 @@
-{stdenv, fetchFromGitHub, cmake, pkgconfig, gtest, rocm-cmake, rocm-runtime, hip
-
-# The test suite takes a long time to build
-, doCheck ? false
+{stdenv, fetchFromGitHub, lib, config, cmake, pkgconfig
+, rocm-cmake, rocm-runtime, hip
+, doCheck ? false, gtest ? null
 }:
+
+assert doCheck -> gtest != null;
+
 stdenv.mkDerivation rec {
   name = "rocprim";
-  version = "3.5.1";
+  version = "3.8.0";
   src = fetchFromGitHub {
     owner = "ROCmSoftwarePlatform";
     repo = "rocPRIM";
     rev = "rocm-${version}";
-    sha256 = "0057i6ww9wgf8z1hvdqnw4fh9qjc3pzx2bzf0ai6qw4ljazabklq";
+    sha256 = "0sfypzcpkknn8m4j3w8wahzgjaa8qir7rxmxywwa3vg7a2a4xmdc";
   };
-  nativeBuildInputs = [ cmake rocm-cmake pkgconfig ]
-    ++ stdenv.lib.optional doCheck gtest;
+  
+  inherit doCheck;
+  
+  nativeBuildInputs = [ cmake rocm-cmake pkgconfig ];
+    
   buildInputs = [ rocm-runtime hip ];
+
+  checkInputs = [ gtest ];
+
   cmakeFlags = [
     "-DCMAKE_CXX_COMPILER=hipcc"
     "-DCMAKE_INSTALL_INCLUDEDIR=include"
+    "-DAMDGPU_TARGETS=${lib.strings.concatStringsSep ";" (config.rocmTargets or ["gfx803" "gfx900" "gfx906"])}"
     "-DBUILD_TEST=${if doCheck then "YES" else "NO"}"
+    "${if doCheck then "-DAMDGPU_TEST_TARGETS=${lib.strings.concatStringsSep ";" (config.rocmTargets or ["gfx803" "gfx900" "gfx906"])}" else ""}"
   ];
+  
   patchPhase = ''
     sed -e '/find_package(Git/,/endif()/d' \
         -e '/download_project(/,/^[[:space:]]*)/d' \
         -i cmake/Dependencies.cmake
-    sed 's,include(cmake/VerifyCompiler.cmake),,' -i CMakeLists.txt
+    #sed 's,include(cmake/VerifyCompiler.cmake),,' -i CMakeLists.txt
+  '';
+  
+  checkPhase = ''
+    ctest
   '';
 }
