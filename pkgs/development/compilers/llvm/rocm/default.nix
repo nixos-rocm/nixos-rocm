6072fce63864f2d4b5558a3ecf857f4331d4a936
Anthony Cowley <acowley@gmail.com>
2020-09-24 00:59:31 -0400

    clang: link compiler-rt to resource-root

--- pkgs/development/compilers/llvm/rocm/default.nix
+++ pkgs/development/compilers/llvm/rocm/default.nix
@@ -16,6 +16,22 @@ in rec {
       rsrc="$out/resource-root"
       mkdir "$rsrc"
       ln -s "${cc}/lib/clang/$clang_version/include" "$rsrc"
+      ln -s "${compiler-rt}/lib" "$rsrc/lib"
+      echo "-resource-dir=$rsrc" >> $out/nix-support/cc-cflags
+      echo "--gcc-toolchain=${stdenv.cc.cc}" >> $out/nix-support/cc-cflags
+      echo "-Wno-unused-command-line-argument" >> $out/nix-support/cc-cflags
+      rm $out/nix-support/add-hardening.sh
+      touch $out/nix-support/add-hardening.sh
+    '';
+  };
+
+  clangNoCompilerRt = wrapCCWith rec {
+    cc = clang-unwrapped;
+    extraBuildCommands = ''
+      clang_version=`${cc}/bin/clang -v 2>&1 | grep "clang version " | grep -E -o "[0-9.-]+"`
+      rsrc="$out/resource-root"
+      mkdir "$rsrc"
+      ln -s "${cc}/lib/clang/$clang_version/include" "$rsrc"
       echo "-resource-dir=$rsrc" >> $out/nix-support/cc-cflags
       echo "--gcc-toolchain=${stdenv.cc.cc}" >> $out/nix-support/cc-cflags
       echo "-Wno-unused-command-line-argument" >> $out/nix-support/cc-cflags
@@ -42,6 +58,6 @@ in rec {
   compiler-rt = callPackage ./compiler-rt.nix {
     inherit version llvm;
     src = "${src}/compiler-rt";
-    stdenv = overrideCC stdenv clang;
+    stdenv = overrideCC stdenv clangNoCompilerRt;
   };
 }
