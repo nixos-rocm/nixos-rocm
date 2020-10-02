6c4cb82b0ff383b521bda7fc5d26f193f09e24d1
Wulfsta <wulfstawulfsta@gmail.com>
2020-10-02 18:54:11 -0400

    miopen: update to 2.7.0

--- pkgs/development/libraries/miopen/default.nix
+++ pkgs/development/libraries/miopen/default.nix
@@ -1,34 +1,40 @@
-{ stdenv, fetchFromGitHub, cmake, pkgconfig, half, openssl, boost, sqlite, bzip2
-, rocm-cmake, rocm-opencl-runtime, rocm-runtime, clang, clang-unwrapped, clang-ocl, miopengemm, rocblas
-, comgr, useHip ? false, hip }:
-assert useHip -> hip != null;
+{ stdenv, fetchFromGitHub, cmake, config, lib, pkgconfig, half, openssl, boost, sqlite, bzip2
+, rocm-cmake, rocm-runtime, clang, clang-unwrapped, hip, miopengemm, rocblas, comgr, clang-ocl
+, use_ocl ? false, rocm-opencl-runtime ? null
+}:
+
+assert use_ocl -> rocm-opencl-runtime != null;
+
 stdenv.mkDerivation rec {
   name = "miopen";
-  version = "2.4.0";
+  version = "2.7.0";
   src = fetchFromGitHub {
     owner = "ROCmSoftwarePlatform";
     repo = "MIOpen";
     rev = "${version}";
-    sha256 = "1qs4zxqza4bg1055fnbyhrvyskbg8f4nc6adlrn6yqq2xin50jsz";
+    sha256 = "1hvjqg1pn52k3zyxi0wlrabi874g5n5pshdnfq5wmyaqr2vy694p";
   };
+
   nativeBuildInputs = [ cmake pkgconfig rocm-cmake ];
-  buildInputs = [ rocm-runtime half openssl boost rocblas miopengemm comgr sqlite bzip2 ]
-    ++ (if useHip then [ hip ] else [rocm-opencl-runtime clang-ocl hip]);
+
+  buildInputs = [ rocm-runtime half openssl boost rocblas miopengemm comgr sqlite bzip2 clang-ocl hip ]
+    ++ lib.optionals use_ocl [ rocm-opencl-runtime ];
 
   cmakeFlags = [
-    "-DCMAKE_PREFIX_PATH=${hip};${clang-ocl}"
+    "-DCMAKE_PREFIX_PATH=${if use_ocl then "${hip};${clang-ocl}" else "${hip}"}"
     "-DCMAKE_INSTALL_INCLUDEDIR=include"
     "-DMIOPEN_USE_ROCBLAS=ON"
     "-DBoost_USE_STATIC_LIBS=OFF"
     "-DMIOPEN_USE_MIOPENGEMM=ON"
     "-DMIOPEN_AMDGCN_ASSEMBLER_PATH=${clang}/bin"
     "-DMIOPEN_OFFLOADBUNDLER_BIN=${clang-unwrapped}/bin/clang-offload-bundler"
-  ] ++ (if useHip
+  ] ++ (if !use_ocl
         then [ # "-DCMAKE_CXX_COMPILER=${clang}/bin/clang++"
                # "-DCMAKE_C_COMPILER=${clang}/bin/clang"
                "-DCMAKE_CXX_COMPILER=${hip}/bin/hipcc"
                "-DCMAKE_C_COMPILER=${hip}/bin/hipcc"
                "-DMIOPEN_BACKEND=HIP"
+               "-DAMDGPU_TARGETS=${lib.strings.concatStringsSep ";" (config.rocmTargets or [ "gfx803" "gfx900" "gfx906" ])}"
                # "-DENABLE_HIP_WORKAROUNDS=YES"
         ]
         else [ "-DMIOPEN_BACKEND=OpenCL"
@@ -36,7 +42,9 @@ stdenv.mkDerivation rec {
                "-DCMAKE_C_COMPILER=${clang}/bin/clang"
                # "-DOPENCL_INCLUDE_DIRS=${rocm-opencl-runtime}/include/opencl2.2"
                # "-DOPENCL_LIB_DIRS=${rocm-opencl-runtime}/lib"
-  ]);
+        ]
+  );
+  
   patchPhase = ''
     sed -e 's,cmake_minimum_required( VERSION 2.8.12 ),cmake_minimum_required( VERSION 3.10 ),' \
         -e 's,\(set( MIOPEN_INSTALL_DIR\).*,\1 ''${CMAKE_INSTALL_PREFIX}),' \
