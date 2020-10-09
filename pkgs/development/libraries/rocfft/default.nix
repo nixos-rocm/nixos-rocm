dcbe7aa0894607013a50ae274ca64c96fedf3c67
Luke V <wulfstawulfsta@gmail.com>
2020-10-09 19:56:00 -0400

    Revert "rocfft: update to 1.0.6"
    This reverts commit 2708e9d77cb7520d041fd0c35ba2eeb9e994e6b7.

--- pkgs/development/libraries/rocfft/default.nix
+++ pkgs/development/libraries/rocfft/default.nix
@@ -1,42 +1,31 @@
-{ stdenv, fetchFromGitHub, fetchpatch, lib, config, cmake, pkgconfig
+{ stdenv, fetchFromGitHub, fetchpatch, cmake, pkgconfig
 , rocm-runtime, rocminfo, clang, hip, rocm-cmake, comgr
-, boost 
-, doCheck ? false, gtest ? null, fftw ? null, fftwFloat ? null 
-}:
-
-assert doCheck -> gtest != null
-               && fftw != null
-               && fftwFloat != null;
-
+, doCheck ? false
+, boost, gtest, fftw, fftwFloat }:
 stdenv.mkDerivation rec {
   name = "rocFFT";
-  version = "3.8.0";
+  version = "3.5.0";
   src = fetchFromGitHub {
     owner = "ROCmSoftwarePlatform";
     repo = "rocFFT";
-    rev = "rocm-${version}";
-    sha256 = "129jvg3jl723wixfnw0w3ljjgjz49jkdwcp80layzlh2xifqm880";
+    rev = "${version}";
+    sha256 = "03cxfx40gg817pm1lknpi7vma17wfqggprxd8vz8hlipxd3cisng";
   };
 
   # Building this package is very RAM intensive: individual clang
   # processes use over 6GB of RAM.
-  #enableParallelBuilding = false;
-  
-  CXXFLAGS = "-D__HIP_PLATFORM_HCC__ -D__HIP__ ";
+  enableParallelBuilding = false;
+  CXXFLAGS = "-D__HIP_PLATFORM_HCC__ -D__HIP__";
 
   nativeBuildInputs = [ cmake rocm-cmake pkgconfig rocminfo ];
-  
-  buildInputs = [ hip rocm-runtime boost comgr ];
-  
-  checkInputs = [ gtest fftwFloat fftw ];
-  
+  buildInputs = [ hip rocm-runtime boost comgr ]
+    ++ stdenv.lib.optionals doCheck [ gtest fftwFloat fftw ];
   cmakeFlags = [
-    "-DUSE_HIP_CLANG=ON"
+    # "-DUSE_HIP_CLANG=YES"
     "-DCMAKE_CXX_COMPILER=${hip}/bin/hipcc"
     # "-DHSA_HEADER=${rocm-runtime}/include"
     # "-DHSA_LIBRARY=${rocm-runtime}/lib/libhsa-runtime64.so"
     "-DCMAKE_INSTALL_INCLUDEDIR=include"
-    "-DAMDGPU_TARGETS=${lib.strings.concatStringsSep ";" (config.rocmTargets or [ "gfx803" "gfx900" "gfx906" ])}"
   ] ++ stdenv.lib.optionals doCheck [
     "-DBUILD_CLIENTS_TESTS=ON"
     "-DBUILD_CLIENTS_BENCHMARKS=ON"
